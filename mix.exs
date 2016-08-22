e676318e547182ca0059d4e920c53d9b8024015f
Kevin Smith <kevin@operable.io>
2016-08-22 10:53:43 -0400

    Bump version for next release

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule FuManchu.Mixfile do
 
   def project do
     [app: :fumanchu,
-     version: "0.13.0",
+     version: "0.14.0",
      elixir: "~> 1.3.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,

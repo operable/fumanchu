24375794d9bf0ee0b7058ee7eecdaa2cace564ba
Julius Beckmann <github@h4cc.de>
2018-04-25 15:59:53 +0200

    Fixing compiler warning.
    `warning: variable "deps" does not exist and is being expanded to "deps()"`

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule FuManchu.Mixfile do
      elixir: "~> 1.3.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps,
+     deps: deps(),
      description: "An (almost) spec-compliant Mustache parser written in Elixir"]
   end
 
